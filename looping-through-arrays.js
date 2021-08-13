7abf095b65faf65d192135646cb6f0569565e56d
k2pbac <krisbachan123@hotmail.com>
2021-08-13 07:52:30 -0400

    test

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,7 +1,8 @@
-const pets = ['cat','dog','rat'];
+const pets = ["cat", "dog", "rat"];
 
-for(var i =0; i< pets.length; i++){
-pets[i] = pets[i] + 's';
+for (var i = 0; i < pets.length; i++) {
+  pets[i] = pets[i] + "s";
 }
 
 console.log(pets);
+//test
